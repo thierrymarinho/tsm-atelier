a13d1196ac38785c651f5f69c19b0396a92cfdee
Thierry <thierrymarinho26@gmail.com>
2026-07-08 21:56:42 -0300

    build: add spotless plugin for java code formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "4.1.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.tm"
@@ -33,4 +34,13 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+spotless {
+	java {
+		importOrder()
+		removeUnusedImports()
+		eclipse()
+		formatAnnotations()
+	}
 }

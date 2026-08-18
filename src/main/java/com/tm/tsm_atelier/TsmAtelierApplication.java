4d6cbad44e752218742a02829d39109700bbca2b
thierry <thierrydev48@gmail.com>
2026-08-18 17:18:05 -0300

    style(core): import EnableSpringDataWebSupport instead of qualifying it inline

--- src/main/java/com/tm/tsm_atelier/TsmAtelierApplication.java
+++ src/main/java/com/tm/tsm_atelier/TsmAtelierApplication.java
@@ -2,11 +2,12 @@ package com.tm.tsm_atelier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@org.springframework.data.web.config.EnableSpringDataWebSupport(pageSerializationMode = org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class TsmAtelierApplication {
 
 	public static void main(String[] args) {
